f086b775173a46f0d4849a1e78d8a5e7cc88ba69
Giedrius <giedriuszeb@gmail.com>
2014-02-19 14:12:12 +0000

    Angular, html

--- FeelFrog/Angular/app/js/app.js
+++ FeelFrog/Angular/app/js/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/index,' {templateUrl: 'partials/input.html', controller: 'MyCtrl'});
+  $routeProvider.when('/output', {templateUrl: 'partials/output.html', controller: 'MyCtrl'});
+  $routeProvider.otherwise({redirectTo: 'localhost:8000/app/partials/view1'});
 }]);
--- FeelFrog/Angular/app/js/controllers.js
+++ FeelFrog/Angular/app/js/controllers.js
@@ -2,10 +2,9 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
+angular.module('myApp.controllers', ['$scope', '$route', '$routeParams', '$location']).
+  controller('MyCtrl', [function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
   }]);
