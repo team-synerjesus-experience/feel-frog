4dc66180d640cb787515e987c7cfb3a3ce72da42
Matthew Summers <matt@wishf.co.uk>
2014-02-20 11:38:00 +0000

    Added date parsing. Hopefully

--- FeelFrog/api/api.py
+++ FeelFrog/api/api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from correlations import correlation, train
+import datetime
 app = Flask(__name__)
 
 @app.route('/v0/get/correlation', methods = ['POST'])
@@ -32,10 +33,16 @@ def query_prediction():
 	return jsonify( { 'prediction': response } ), 201
 
 @app.route('/v0/get/activites/between/<start>/and/<end>', method = ['POST'])
-def get_activities():
+def get_activities(start, end):
 	if not request.json or
 	   not 'user' in request.json: # complete 
 	   	abort(400)
+
+	start_date = date_parse(start)
+	end_date = date_parse(end)
+
+	if start_date is None or end_date is None:
+		abort(400)
 
 	# get intervals out
 
@@ -85,3 +92,11 @@ def add_mood():
 		'success' : success_state
 	}
 	return jsonify(response), 201 # change 201 depending on success_state
+
+# Uses format string %Y%m%d%H%M
+def date_parse(input):
+		try:
+			return datetime.strptime(input, "%Y%m%d%H%M")
+		except ValueError:
+			return None
+
